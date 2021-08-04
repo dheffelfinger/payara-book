0167c5755330d1591b2a19bd8514ea9bd03c35e8
David R. Heffelfinger <dheffelfinger@ensode.com>
2021-08-04 18:41:38 -0400

    Added sample logic for handling HTTP PUT, POST, DELETE and PATCH requests.

--- ch02/microservice/src/main/java/com/ensode/microservice/SampleRestFulService.java
+++ ch02/microservice/src/main/java/com/ensode/microservice/SampleRestFulService.java
@@ -1,9 +1,11 @@
 package com.ensode.microservice;
 
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
@@ -13,15 +15,56 @@ import javax.ws.rs.core.MediaType;
 @Path("sample")
 public class SampleRestFulService {
 
-  @Context
-  private UriInfo context;
-
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public String getJson() {
+  public String processGetRequest() {
 
     String json = "{"
-            + "\"msg\":\"Hello, world!\""
+            + "\"msg\":\"Service processed HTTP GET request!\""
+            + "}";
+
+    return json;
+  }
+
+  @PUT
+  @Produces(MediaType.APPLICATION_JSON)
+  public String processPutRequest() {
+
+    String json = "{"
+            + "\"msg\":\"Service processed HTTP PUT request!\""
+            + "}";
+
+    return json;
+  }
+
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  public String processPostRequest() {
+
+    String json = "{"
+            + "\"msg\":\"Service processed HTTP POST request!\""
+            + "}";
+
+    return json;
+  }
+
+  @DELETE
+  @Produces(MediaType.APPLICATION_JSON)
+  public String processDeleteRequest() {
+
+    String json = "{"
+            + "\"msg\":\"Service processed HTTP DELETE request!\""
+            + "}";
+
+    return json;
+  }
+
+  @PATCH
+  @Produces(MediaType.APPLICATION_JSON)
+  public String processPatchRequest() {
+
+    String json = "{"
+            + "\"msg\":\"Service processed HTTP PATCH request!\""
             + "}";
 
     return json;
