c07e97fcca1330605e580e87ec23134567ec2474
David R. Heffelfinger <dheffelfinger@ensode.com>
2021-08-31 18:44:15 -0400

    updated example illustrating CDI dependency injection.

--- ch04/cdi-dependency-injection/src/main/java/com/ensode/dependencyinjection/CdiService.java
+++ ch04/cdi-dependency-injection/src/main/java/com/ensode/dependencyinjection/CdiService.java
@@ -1,10 +1,6 @@
 package com.ensode.dependencyinjection;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,7 +8,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@RequestScoped
 @Path("cdiservice")
 public class CdiService {
 
@@ -20,16 +15,6 @@ public class CdiService {
   private CountryLookup countryLookup;
 
   private static final Logger LOGGER = Logger.getLogger(CdiService.class.getName());
-
-  @PostConstruct
-  public void init() {
-    LOGGER.log(Level.INFO, "init() method called");
-  }
-
-  @PreDestroy
-  public void cleanup() {
-    LOGGER.log(Level.INFO, "cleanup() method called");
-  }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
