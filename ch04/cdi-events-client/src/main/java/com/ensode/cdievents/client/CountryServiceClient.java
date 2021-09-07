134bf4cd96f4ece579924ce6975f0db2a59e8151
David R. Heffelfinger <dheffelfinger@ensode.com>
2021-09-07 19:24:12 -0400

    minor cleanup

--- ch04/cdi-events-client/src/main/java/com/ensode/cdievents/client/CountryServiceClient.java
+++ ch04/cdi-events-client/src/main/java/com/ensode/cdievents/client/CountryServiceClient.java
@@ -1,5 +1,6 @@
 package com.ensode.cdievents.client;
 
+import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
@@ -8,20 +9,21 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
 public class CountryServiceClient {
-  
+
+  private static final Logger LOGGER = Logger.getLogger(CountryServiceClient.class.getName());
+
   @Inject
   @RestClient
   private CountryService cdiService;
-  
-  public void updateCountries(@Observes @Initialized(ApplicationScoped.class) Object object){
+
+  public void updateCountries(@Observes @Initialized(ApplicationScoped.class) Object object) {
     Country testCountry = new Country("TS", "Test");
     Country albanya = new Country("AL", "Albanya");
     Country albania = new Country("AL", "Albania");
-    
+
     cdiService.updateCountry(testCountry);
     cdiService.deleteCountry(testCountry.getAbbreviation());
     cdiService.updateCountry(albanya);
     cdiService.updateCountry(albania);
-    
   }
 }
