135d0b6a6e516f4cc29703ed25a1e1cb0e88fc2e
David R. Heffelfinger <dheffelfinger@ensode.com>
2021-09-07 18:10:39 -0400

    Removed unnecessary targets from qualifier annotations.

--- ch04/cdi-events/src/main/java/com/ensode/cdievents/qualifier/Updated.java
+++ ch04/cdi-events/src/main/java/com/ensode/cdievents/qualifier/Updated.java
@@ -1,16 +1,13 @@
 package com.ensode.cdievents.qualifier;
 
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.inject.Qualifier;
 
 @Qualifier
-@Retention(RUNTIME)
-@Target({METHOD, FIELD, PARAMETER, TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
 public @interface Updated {
 }
