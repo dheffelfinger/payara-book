8610d9934a667b75b5158624702c3cc08682bf0d
David R. Heffelfinger <dheffelfinger@ensode.com>
2021-10-18 10:59:31 -0400

    Initial fault tolerance example.

---
+++ ch07/mp-fault-tolerance-example/src/main/java/com/ensode/faulttolerance/ApplicationConfig.java
@@ -0,0 +1,7 @@
+package com.ensode.faulttolerance;
+
+import javax.ws.rs.core.Application;
+
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+}
---
+++ ch07/mp-fault-tolerance-example/src/main/java/com/ensode/faulttolerance/FaulToleranceExampleResource.java
@@ -0,0 +1,27 @@
+package com.ensode.faulttolerance;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Asynchronous;
+
+
+@RequestScoped
+@Path("faulttoleranceexample")
+public class FaulToleranceExampleResource {
+
+  @Asynchronous
+  @GET
+  @Path("async")
+  @Produces(MediaType.TEXT_PLAIN)
+  public CompletionStage<Integer> getAsynchronousValue() throws InterruptedException {
+    TimeUnit.SECONDS.sleep(5);
+    return CompletableFuture.completedStage(42);
+  }
+
+}
