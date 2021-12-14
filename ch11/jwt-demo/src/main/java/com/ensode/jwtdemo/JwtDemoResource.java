ea70fde85a3a0a20886632094862dc9ca5e12c83
David R. Heffelfinger <dheffelfinger@ensode.com>
2021-12-14 18:34:46 -0500

    updated example with sample code illustrating how to retrieve data from a JSON Web token

--- ch11/jwt-demo/src/main/java/com/ensode/jwtdemo/JwtDemoResource.java
+++ ch11/jwt-demo/src/main/java/com/ensode/jwtdemo/JwtDemoResource.java
@@ -1,21 +1,47 @@
 package com.ensode.jwtdemo;
 
+import java.security.Principal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
-//need to make JAX-RS endpont a request scoped CDI bean, otherwise security will not "kick in"
 @RequestScoped
 @Path("jwtdemo")
 public class JwtDemoResource {
+
+  private static final Logger LOGGER = Logger.getLogger(JwtDemoResource.class.getName());
+
+  @Inject
+  private JsonWebToken jsonWebToken;
 
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   @RolesAllowed({"chief"})
   public String secured() {
+
+    LOGGER.log(Level.INFO, String.format("Audience: %s", jsonWebToken.getAudience()));
+    LOGGER.log(Level.INFO, String.format("Expiration Time: %s", jsonWebToken.getExpirationTime()));
+    LOGGER.log(Level.INFO, String.format("Groups: %s", jsonWebToken.getGroups()));
+    LOGGER.log(Level.INFO, String.format("Issued at time: %s", jsonWebToken.getIssuedAtTime()));
+    LOGGER.log(Level.INFO, String.format("Issuer: %s", jsonWebToken.getIssuer()));
+    LOGGER.log(Level.INFO, String.format("Name: %s", jsonWebToken.getName()));
+    LOGGER.log(Level.INFO, String.format("Raw Token: %s", jsonWebToken.getRawToken()));
+    LOGGER.log(Level.INFO, String.format("Subject: %s", jsonWebToken.getSubject()));
+    LOGGER.log(Level.INFO, String.format("Token ID: %s", jsonWebToken.getTokenID()));
+
+    LOGGER.log(Level.INFO, "--- Begin Token Claims");
+    jsonWebToken.getClaimNames().forEach(c
+            -> LOGGER.log(Level.INFO,
+                    String.format("%s: %s", c, jsonWebToken.claim(c).orElse(""))));
+    LOGGER.log(Level.INFO, "--- End Token Claims");
+
     return "Secured endpoint accessed successfully\n";
   }
 }
