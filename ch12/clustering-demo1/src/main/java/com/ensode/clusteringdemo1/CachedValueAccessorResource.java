9c27aec158e92b71402297338c16666516c0a77b
David R. Heffelfinger <dheffelfinger@ensode.com>
2021-12-21 19:05:01 -0500

    corrected typo

--- ch12/clustering-demo1/src/main/java/com/ensode/clusteringdemo1/CachedValueAccessorResource.java
+++ ch12/clustering-demo1/src/main/java/com/ensode/clusteringdemo1/CachedValueAccessorResource.java
@@ -9,7 +9,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("cachedcalueaccessor")
+@Path("cachedvalueaccessor")
 public class CachedValueAccessorResource {
 
   @Inject
